e1f1709acba551e1264b2ae48feadb2fa51e3fd3
Jonathan Harmon <jonathan@jonathanharmon.com>
2023-05-03 09:59:32 -0400

    create server and import cors express stripe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -33,7 +35,7 @@ export class CartComponent implements OnInit{
     'action'
   ]
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private http: HttpClient) { }
   
   ngOnInit(): void {
     this.dataSource = this.cart.items;
@@ -61,6 +63,17 @@ export class CartComponent implements OnInit{
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) => {
+      let stripe = await loadStripe('pk_test_51N3fgODs93hvqyjXpymq6cipA0YCUEJs7qy8f2Xl27o5h05LhKFJLamyCEUESW7hEYpAPvUAuufJzynldlbIVFqj00e89AUVVP');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    });
   }
 
 }
